ef55330f4ccb23c20de67d937df687d798305153
mda17ls <102365277+mda17ls@users.noreply.github.com>
2022-03-30 12:21:22 +0000

    -

---
+++ getsummary.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# load our data fileand assign it
+survey_df = pd.read_csv("surveys.csv")
+
+# describe weight column 
+sec_weight = surveys_df[]"weight"].describe()
+
+# print
+print(desc_weight)
